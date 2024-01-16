e7acaa6598096936959da1de6a0938159b65cf14
Mihai Voina <mihaitza1058@yahoo.com>
2024-01-16 16:52:46 +0200

    improved gmail message  styling

--- resources/views/verification/register.blade.php
+++ resources/views/verification/register.blade.php
@@ -1,52 +1,92 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    <style>
+        body {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            margin: 0;
+           
+            font-family: Arial, sans-serif;
+        }
 
-        <title>Laravel</title>
+        .email-container {
+            text-align: center;
+            max-width: 400px;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 8px;
+         
+         
+   
+        }
 
-       <link rel="stylesheet" href="{{asset('fontawesome-free-6.5.1-web\css\all.min.css')}}">
-        @vite('resources/css/app.css')
-        <script src="//unpkg.com/alpinejs" defer></script>
+      
+
+        .title {
+            font-size: 24px;
+            font-weight: bold;
+            margin-bottom: 20px;
+        }
+
+        .content {
+            font-size: 16px;
+            margin-bottom: 20px;
+        }
+
+        .user-info {
+            text-align: left;
+            margin-bottom: 20px;
+            font-size: 16px;
+        }
+
+        .user-info p {
+            margin: 8px 0;
+            
+        }
+
+        .button {
+            display: inline-block;
+            padding: 10px 20px;           
+            text-decoration: none;
+            border-radius: 5px;
+            transition: background-color 0.3s;
+        }
+
        
 
-    </head>
-    <body >
-        <x-flashMessages />
-
-        <div class="container mx-auto p-8">
-            <div class="bg-white max-w-md mx-auto p-8 rounded shadow-md">
-                <img src="{{ url('/images/logo.png') }}" alt="Logo" class="mx-auto mb-4">
-                <h1 class="text-2xl font-semibold mb-4">Register Here</h1>
-                <p class="text-gray-700">
-                    Thanks for registering! Please check your email click the button below to complete your registration:
-                </p>
-                <p class="mt-4 text-gray-700">
-                    Email: {{ $email }}
-                </p>
-                <p class="mt-4 text-gray-700">
-                    Username: {{ $userName }}
-                </p>
-
-                <p class="mt-4 text-gray-700">
-                    Password: {{ $plainPassword }}
-                </p>
-            
-                <a href="http://127.0.0.1:8000/" style="color: #ffffff;" class="mt-2 mb-2 inline-block bg-[#020617] hover:bg-[#1f2937] text-white px-6 py-2 rounded">
-                    Register
-                </a>
-
-                <p class="mt-4 text-gray-700">
-                    Thanks for choosing {{ config('app.name') }}!
-                </p>
-                
+        .footer {
+            font-size: 14px;
+            color: #666;
+        }
+    </style>
+</head>
+<body >
+    <div class="container" >
+        <div class="email-container">
+            <div class="header">
+                <h1 class="title">Register Here</h1>
             </div>
+            <p class="content">
+                Thanks for registering! Please check your email and click the button below to complete your registration:
+            </p>
+            <div class="user-info">
+                <p>Email: {{ $email }}</p>
+                <p>Username: {{ $userName }}</p>
+                <p>Password: {{ $plainPassword }}</p>
+            </div>
+            <a href="http://127.0.0.1:8000/" class="button" style="color:white; background-color:black; ">Register</a>
+            <p class="footer">
+                Thanks for choosing {{ config('app.name') }}!
+            </p>
+            <hr>
+            <p class="footer">Copyright &copy; All Rights reserved</p>
         </div>
     </div>
-    
-    
-     
-   
-    </body>
+</body>
 </html>
