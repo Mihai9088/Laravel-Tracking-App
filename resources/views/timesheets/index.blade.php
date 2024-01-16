b4102adf75dc2a58ad224daa3d6864e84019dbd2
Mihai Voina <mihaitza1058@yahoo.com>
2024-01-16 18:56:15 +0200

    improved styling on timesheets page

--- resources/views/timesheets/index.blade.php
+++ resources/views/timesheets/index.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,6 +10,7 @@
     <script src="//unpkg.com/alpinejs" defer></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
 </head>
+
 <body class="bg-gray-100 font-sans">
     @include('navbar')
     <x-flashMessages />
@@ -18,21 +20,21 @@
                 <h1 class="text-3xl font-semibold mb-4">Timesheets</h1>
                 <div class="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0 md:space-x-4">
                     <div class="flex items-center space-x-4">
-                        <a href="/timesheets/create" class="bg-[#020617] hover:bg-[#1f2937] text-white px-6 py-2 rounded-md  ">
-                            Create Timesheet
-                        </a>
-                        
-                        <a href="{{ route('timesheets.export.csv') }}" class="bg-[#020617] hover:bg-[#1f2937] text-white py-2 px-4 rounded">Export CSV</a>
+                        <a href="/timesheets/create"
+                            class="bg-[#020617] hover:bg-[#1f2937] text-white px-6 py-2 rounded-md  ">Create
+                            Timesheet</a>
+                        <a href="{{ route('timesheets.export.csv') }}"
+                            class="bg-[#020617] hover:bg-[#1f2937] text-white py-2 px-4 rounded">Export CSV</a>
                     </div>
-        
                     <div class="flex items-center space-y-4 md:space-y-0 md:space-x-4">
-                        <label for="date_filter" class="block text-sm font-medium text-gray-700">Filter by date</label>
-        
-                        <form action="{{ route('timesheets.filter') }}" method="get" class="flex items-center space-y-4 md:space-y-0 md:space-x-4">
-                            
+                        <label for="date_filter"
+                            class="block text-sm font-medium text-gray-700">Filter by date</label>
+                        <form action="{{ route('timesheets.filter') }}" method="get"
+                            class="flex items-center space-y-4 md:space-y-0 md:space-x-4">
                             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                                 <div>
-                                    <select name="date_filter" class="form-select bg-gray-100 border border-gray-300 p-2 rounded">
+                                    <select name="date_filter"
+                                        class="form-select bg-gray-100 border border-gray-300 p-2 rounded">
                                         <option value="">All Dates</option>
                                         <option value="today">Today</option>
                                         <option value="yesterday">Yesterday</option>
@@ -44,8 +46,8 @@
                                         <option value="last-year">Last Year</option>
                                     </select>
                                 </div>
-        
-                                <button type="submit" class="bg-[#020617] hover:bg-[#1f2937] text-white p-2 rounded">Filter</button>
+                                <button type="submit"
+                                    class="bg-[#020617] hover:bg-[#1f2937] text-white p-2 rounded">Filter</button>
                             </div>
                         </form>
                     </div>
@@ -53,56 +55,82 @@
             </div>
             <div class="p-6 overflow-x-auto">
                 <h2 class="text-2xl font-semibold mb-6">Timesheets List</h2>
-
                 @if(count($timeSheets) > 0)
-                    <table class="min-w-full divide-y divide-gray-200">
-                        <thead>
+                <div class="overflow-x-auto">
+                    <table
+                        class="min-w-full sm:border-collapse sm:border-gray-200 divide-y divide-gray-200 overflow-hidden sm:rounded-lg">
+                        <thead class="bg-gray-50">
                             <tr>
-                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Project</th>
-                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Task</th>
-                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Date In</th>
-                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Time In</th>
-                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Date Out</th>
-                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Time Out</th>
-                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Hours worked</th>
-                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Rate</th>
-                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Description</th>
-                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Edit</th>
-                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Delete</th>
+                                <th
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Project</th>
+                                <th
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Task</th>
+                                <th
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Date In</th>
+                                <th
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Time In</th>
+                                <th
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Date Out</th>
+                                <th
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Time Out</th>
+                                <th
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Hours worked</th>
+                                <th
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Rate</th>
+                                <th
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Description</th>
+                                <th
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Edit</th>
+                                <th
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Delete</th>
                             </tr>
                         </thead>
                         <tbody class="bg-white divide-y divide-gray-200">
                             @foreach($timeSheets as $timesheet)
-                                <tr>
-                                    <td class="px-6 py-4 whitespace-no-wrap">{{ $timesheet->project }}</td>
-                                    <td class="px-6 py-4 whitespace-no-wrap">{{ $timesheet->task }}</td>
-                                    <td class="px-6 py-4 whitespace-no-wrap">{{ $timesheet->date_in }}</td>
-                                    <td class="px-6 py-4 whitespace-no-wrap">{{ $timesheet->time_in }}</td>
-                                    <td class="px-6 py-4 whitespace-no-wrap">{{ $timesheet->date_out }}</td>
-                                    <td class="px-6 py-4 whitespace-no-wrap">{{ $timesheet->time_out }}</td>
-                                    <td class="px-6 py-4 whitespace-no-wrap">{{ $timesheet->hours_worked }}</td>
-                                    <td class="px-6 py-4 whitespace-no-wrap">{{ $timesheet->rate }}</td>
-                                    <td class="px-6 py-4 whitespace-no-wrap">{{ $timesheet->description }}</td>
-                                    <td class="px-6 py-4 whitespace-no-wrap"><a href="{{ route('timesheets.edit', $timesheet->id)}}" class="text-blue-500">Edit</a></td>
-                                    <td class="px-6 py-4 whitespace-no-wrap">
-                                        <form action="{{ route('timesheets.destroy', $timesheet->id) }}" method="post">
-                                            @csrf
-                                            @method('delete')
-                                            <button type="submit" class="text-red-500 hover:cursor-pointer">Delete</button>
-                                        </form>
-                                    </td>
-                                </tr>
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap">{{ $timesheet->project }}</td>
+                                <td class="px-6 py-4 whitespace-nowrap">{{ $timesheet->task }}</td>
+                                <td class="px-6 py-4 whitespace-nowrap">{{ $timesheet->date_in }}</td>
+                                <td class="px-6 py-4 whitespace-nowrap">{{ $timesheet->time_in }}</td>
+                                <td class="px-6 py-4 whitespace-nowrap">{{ $timesheet->date_out }}</td>
+                                <td class="px-6 py-4 whitespace-nowrap">{{ $timesheet->time_out }}</td>
+                                <td class="px-6 py-4 whitespace-nowrap">{{ $timesheet->hours_worked }}</td>
+                                <td class="px-6 py-4 whitespace-nowrap">{{ $timesheet->rate }}</td>
+                                <td class="px-6 py-4 whitespace-nowrap">{{ $timesheet->description }}</td>
+                                <td class="px-6 py-4 whitespace-nowrap"><a
+                                        href="{{ route('timesheets.edit', $timesheet->id)}}" class="text-blue-500">Edit</a>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <form action="{{ route('timesheets.destroy', $timesheet->id) }}" method="post">
+                                        @csrf
+                                        @method('delete')
+                                        <button type="submit" class="text-red-500 hover:cursor-pointer">Delete</button>
+                                    </form>
+                                </td>
+                            </tr>
                             @endforeach
                         </tbody>
                     </table>
-                    {{ $timeSheets->links() }}
+                </div>
+                {{ $timeSheets->links() }}
                 @else
-                    <p class="text-gray-500">There are no timesheets available.</p>
+                <p class="text-gray-500">There are no timesheets available.</p>
                 @endif
             </div>
         </div>
     </div>
-
     @include('footer')
 </body>
+
 </html>
